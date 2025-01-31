6a0686260024348be2642d06239228b7bdb45d7c
brain188 <tendongnkengafac@gmail.com>
2025-01-31 01:40:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def get_info():
+    response = {
+        "email" : "tendongnkengafac@gmail.com",
+        "current_datetime" : datetime.utcnow().isoformat() + "Z",
+        "github_url" :"https://github.com/brain188/Public-API.git"
+    }
+    
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
